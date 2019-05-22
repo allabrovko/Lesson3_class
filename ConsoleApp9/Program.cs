ff6af2569e044c1b81abde50c95f17584b59024b
joinThe <stadnikgleb@gmail.com>
2019-05-22 20:15:56 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вычисление частного");
+            double a = 12, b = 10, result;
+            if (b == 0)
+                Console.WriteLine("делить на 0 нельзя");
+            else
+            {
+                result = a / b;
+                Console.WriteLine($"Result is: {result}");
+
+            }
+            
+             
+        }
+    }
+}
+
+
+
